50692cee57a2b5760446660c2ca7eaa4a439979a
zachwooddoughty <zachwooddoughty@gmail.com>
2015-07-17 20:40:16 -0700

    playing around with pugbnb

--- assets/js/demo/render.js
+++ assets/js/demo/render.js
@@ -1,7 +1,32 @@
 !function(window, document, $, Pug) {
   var ListView = infinity.ListView,
       ListItem = infinity.ListItem,
-      _ = require('underscore'),
+      _ = require('underscore');
+
+  var Posts = [];
+
+  function getPost(i) {
+    if (Posts.length >= i){
+        return Posts[i];
+    } else {
+        Posts += loadPosts(Posts.length, i);
+        return getPost(i);
+    }
+  }
+
+  function loadPosts(start, end) {
+    $.ajax({
+      url: 'posts',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({posts: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error('comments.json', status, err.toString());
+      }.bind(this)
+    }); 
+  }
       Pugs = Pug.images,
       PugNames = Pug.names,
       PugTaglines = Pug.taglines,
@@ -10,20 +35,18 @@
   var pugTemplate = _.template($('#demo-pug-template').html()),
       doublePugTemplate = _.template($('#demo-double-pug-template').html());
 
-  var columns = $('.list-view'),
+  var column = $('#list-view'),
       pugCount = 0;
 
-  columns.each(function() {
-    var listView = new ListView($(this), {
-      lazy: function() {
-        $(this).find('.pug').each(function() {
-          var $ref = $(this);
-          $ref.attr('src', $ref.attr('data-original'));
-        });
-      }
-    });
-    $(this).data('listView', listView);
+  var listView = new ListView(column, {
+    lazy: function() {
+      $(this).find('.pug').each(function() {
+        var $ref = $(this);
+        $ref.attr('src', $ref.attr('data-original'));
+      });
+    }
   });
+  column.data('listView', listView);
 
 
   function pug() {
@@ -76,19 +99,7 @@
 
 
   function row() {
-    var index, colIndex, length, $minCol, $currCol, $pug;
-
-    for(index = 0, length = columns.length; index < length; index++) {
-
-      for(colIndex = 0; colIndex < length; colIndex++) {
-        $currCol = $(columns[colIndex]);
-
-        if(!$minCol) $minCol = $currCol;
-        else $minCol = $minCol.height() > $currCol.height() ? $currCol : $minCol;
-      }
-
-      $minCol.data('listView').append(pug());
-    }
+      column.data('listView').append(pug());
   }
 
   function pb(num) {
--- server.py
+++ server.py
@@ -8,9 +8,13 @@
 # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import json
 import os
+import json
+import requests
 from flask import Flask, Response, request, send_from_directory
+
+if not os.environ.get("dbo-userid"):
+    import secrets
 
 app = Flask(__name__, static_url_path='', static_folder='')
 app.debug = True
@@ -20,6 +24,33 @@ app.add_url_rule('/', 'root', lambda: app.send_static_file('demo-on.html'))
 @app.route('/inf')
 def inf():
     return send_from_directory('', 'demo-on.html')
+
+
+class SessionAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers['SessionKey'] = self.token
+        return r
+
+    def __repr__(self):
+        return json.dumps({'SessionKey': self.token})
+
+
+@app.route('/posts')
+def get_posts():
+    start = request.args.get('start', 0)
+    end = request.args.get('end', 50)
+
+    userid = os.environ.get("dbo-userid")
+    sessionKey = os.environ.get("dbo-sessionKey")
+    url = "http://discoverboard.com/api/users/%s/latest?start=%s&end=%s" % (userid, start, end)
+    auth = SessionAuth(sessionKey)
+    r = requests.get(url, auth=auth, headers={"User-Agent": "DBoT"})
+    response = Response(json.dumps(r.json()), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+    return response
+
 
 @app.route('/comments.json', methods=['GET', 'POST'])
 def comments_handler():
